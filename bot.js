e807cb1d18e055a48592adc3b86e10ad2526a3b4
Austin Jackson <austinjckson@gmail.com>
2017-06-25 01:26:36 -0500

    responding to proper curly braces

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); //get the environment variables described in .env
 const TelegramBot = require('node-telegram-bot-api');
 const logger = require('au5ton-logger');
+const util = require('util');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -13,8 +14,39 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
+  /*check if message is a query
+  a messages is a query if:
+  - there is exactly 1 `{` per message
+  - there is exactly 1 `}` per message
+  - the message matches the regex: \{([^)]+)\}
+
+  i feel like this could be refractored so please feel free to shit on my code
+  */
+  let l_cnt = 0;
+  let r_cnt = 0;
+  for(let i = 0; i < msg.text.length; i++) {
+      //Correctly tally the braces
+      if(msg.text.charAt(i) === '{') {
+          l_cnt++;
+      }
+      else if(msg.text.charAt(i) === '}') {
+          r_cnt++;
+      }
+
+      //breaking condition
+      if(l_cnt > 1 || r_cnt > 1) {
+          break;
+      }
+  }
+  if(l_cnt === 1 && r_cnt === 1) {
+      let attempt = msg.text.match(/\{([^)]+)\}/);
+      if(attempt !== null) {
+          bot.sendMessage(chatId, util.inspect(attempt));
+      }
+  }
+
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  //bot.sendMessage(chatId, 'Received your message');
 });
 
 logger.log('Bot active.');
