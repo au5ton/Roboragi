15ceb2c488535044f5bf5a6dfe49b0c35885db5b
Austin Jackson <austinjckson@gmail.com>
2017-06-26 05:46:16 -0500

    i have no idea what I'm doing

--- roboruri/main.py
+++ roboruri/main.py
@@ -4,9 +4,11 @@ Roboruri Telegram bot
 
 import telebot
 import os
+import Acerola
 
 from os.path import join, dirname
 from dotenv import load_dotenv
+from pprint import pprint
 
 dotenv_path = join(dirname(__file__),'..', '.env')
 load_dotenv(dotenv_path)
@@ -15,4 +17,6 @@ print(os.environ.get('MAL_USER'))
 
 
 bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_TOKEN'))
+#anime =
 print(bot.get_me())
+pprint(Acerola)
