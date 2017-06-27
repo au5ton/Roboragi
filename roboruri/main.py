24812991b15fae1b234e8ead811766b373b66887
Austin Jackson <austinjckson@gmail.com>
2017-06-26 21:01:09 -0500

    Successfully implemented basic Acerola integration

--- roboruri/main.py
+++ roboruri/main.py
@@ -7,17 +7,39 @@ import os
 import math
 import re
 import colorama
+import json
 
-from Acerola import Acerola
+from Acerola import Acerola, DataSource
 from os.path import join, dirname
 from dotenv import load_dotenv
 from pprint import pprint
 from base64 import standard_b64encode
 from random import randint
+from configparser import ConfigParser
 
-# load .env variables into the os.environ
-dotenv_path = join(dirname(__file__),'..', '.env')
-load_dotenv(dotenv_path)
+# load config.ini
+config = ConfigParser()
+config.read('config.ini')
+
+# initialize Acerola
+acerola = Acerola(config)
+acerola.anime.add_source(DataSource.KITSU,
+                         DataSource.ANILIST,
+                         DataSource.MAL,
+                         DataSource.ANIMEPLANET,
+                         DataSource.ANIDB)
+
+acerola.manga.add_source(DataSource.KITSU,
+                         DataSource.ANILIST,
+                         DataSource.MAL,
+                         DataSource.ANIMEPLANET,
+                         DataSource.MANGAUPDATES)
+
+acerola.light_novel.add_source(DataSource.KITSU,
+                               DataSource.ANILIST,
+                               DataSource.MAL,
+                               DataSource.ANIMEPLANET,
+                               DataSource.MANGAUPDATES)
 
 # make colorama work on Windows
 colorama.init(convert=True,autoreset=True)
@@ -26,13 +48,10 @@ STYLE['ERROR'] = colorama.Fore.RED + colorama.Back.BLACK
 STYLE['WARN'] = colorama.Fore.YELLOW + colorama.Back.BLACK
 STYLE['SUCCESS'] = colorama.Fore.GREEN
 
-bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_TOEN'))
-# anime = Acerola({
-#     'MyAnimeList': {
-#         'Auth':  standard_b64encode((os.environ.get('MAL_USER')+':'+os.environ.get('MAL_PASSWORD')).encode()),
-#         'UserAgent:': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-#     }
-# })
+# initialize telegram bot
+bot = telebot.TeleBot(config['Telegram']['BotToken'])
+
+# startup checks
 print('Bot active. Performing startup checks.')
 print(STYLE['WARN']+'Is our Telegram token valid?')
 try:
@@ -43,12 +62,27 @@ except telebot.apihelper.ApiException:
 except Exception:
     print(STYLE['ERROR']+'Unexpected error:', sys.exc_info()[0])
     exit()
+print(STYLE['WARN']+'Is our Acerola configuration good?')
+print(STYLE['WARN']+'    Query: \'KonoSuba\'')
+try:
+    results = acerola.anime.consolidate(acerola.anime.search_closest('KonoSuba'))
+    print(STYLE['SUCCESS'])
+    print(STYLE['SUCCESS']+'Acerola successfully retrieved sample anime.')
+    if hasattr(results, 'title_romaji'):
+        print(STYLE['SUCCESS']+'    Romanji: '+results.title_romaji)
+    if hasattr(results, 'title_english'):
+        print(STYLE['SUCCESS']+'    English: '+results.title_english)
+    if hasattr(results, 'title_japanese'):
+        print(STYLE['SUCCESS']+'    Native: '+results.title_japanese)
+except Exception:
+    print(STYLE['ERROR']+'Acerola configration has problems:', sys.exc_info()[0])
+    exit()
 
 
-
+"""
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, 'I reply with links to animes with the following format: {anime TV}, [anime OVA + Movies], |exact title|, <manga>')
+    bot.send_message(message.chat.id, 'I reply with links to animes with the following format: {anime TV}, [anime OVA + Movies], |exact title|, <manga>')
 
 @bot.message_handler(content_types=['text'])
 def on_text_message(msg):
@@ -88,4 +122,5 @@ def on_text_message(msg):
         attempt = pat['pipes'].search(msg.text)
         bot.reply_to(msg, 'pipes: '+attempt[1])
 
-bot.polling()
+        bot.polling()
+"""
