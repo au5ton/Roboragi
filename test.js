44f1936ef08bf44a61c18a646e289a52c1993d16
Austin Jackson <austinjckson@gmail.com>
2017-08-08 20:24:33 -0500

    tvdb api probing

--- test.js
+++ test.js
@@ -175,13 +175,24 @@ const non_empty = (val) => {
 //     .then(response => { /* process data */ })
 //     .catch(error => { /* handle error */ });
 
-const imdb = require('imdb-api');
-const token = {apiKey: process.env.OMDB_API_KEY};
-imdb.search({
-    title: process.argv[2]
-}, token).then((results)=> {
-    logger.log(results.results);
-}).catch(console.log);
+// const imdb = require('imdb-api');
+// const token = {apiKey: process.env.OMDB_API_KEY};
+// imdb.search({
+//     title: process.argv[2]
+// }, token).then((results)=> {
+//     logger.log(results.results);
+// }).catch(console.log);
+
+const TVDB = require('node-tvdb');
+const tvdb = new TVDB(process.env.THETVDB_API_KEY);
+
+tvdb.getSeriesByName(process.argv[2])
+    .then((response) => {
+        for(let i in response) {
+            logger.log(response[i]['seriesName'], ' (',response[i]['id'],')');
+        }
+    })
+    .catch(logger.error);
 
 
 
